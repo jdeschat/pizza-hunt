babdbcaf0861673229a9dca8744edcd469b3a67e
Joelle DesChatelets <jdeschat@gmail.com>
2021-10-18 11:18:31 -0400

    add data validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,11 +5,16 @@ const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema(
     {
         pizzaName: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createdBy: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
+
         createdAt: {
             type: Date,
             default: Date.now,
@@ -17,6 +22,8 @@ const PizzaSchema = new Schema(
         },
         size: {
             type: String,
+            required: true,
+            enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
             default: 'Large'
         },
         toppings: [],
